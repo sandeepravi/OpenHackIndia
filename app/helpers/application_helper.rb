b854bdcade250b5a637103c5f7a6c01f3df88f6d
Sandeep <sandeep.ravichandran@sourcebits.com>
2012-08-11 13:40:08 +0530

    Added some styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def random_bg
+    BACKGROUNDS.sample
+  end
 end
---
+++ config/initializers/backgrounds.rb
@@ -0,0 +1,4 @@
+BACKGROUNDS = ["kodaikanal.jpeg", 
+               "Mumbai_skyline_B&W.jpeg",
+               "Berijam Lake Kodaikanal.jpeg",
+               "chansal1.jpeg"]
