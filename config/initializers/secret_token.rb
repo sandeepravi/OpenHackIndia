8b303467be2cb07ad736d6f396e472befd467c31
Sandeep <sandeep.ravichandran@sourcebits.com>
2012-08-11 12:49:32 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trippy::Application.config.secret_token = 'd32aa689b17dbc30abf88463d1d544cbfb86d04e21007d3ed42849bf32b145878ccb2df935ce21e10bbf32a8d8c8973e9cad0f55661ccf3348312df6684588e6'
