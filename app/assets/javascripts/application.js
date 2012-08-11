60cf24cccb7fbaf69e9f57bf8d0935e7c569847c
Sandeep <sandeep.ravichandran@sourcebits.com>
2012-08-11 16:50:15 +0530

    styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.tinycarousel.min.js
 //= require twitter/bootstrap
 //= require_tree .
+
+$(document).ready(function(){				
+  $('#slider-code').tinycarousel({ pager: true });
+});
