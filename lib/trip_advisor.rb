fa49ca39aabb2346ca627a2f92c2604892abd970
bijendra <bijendra@sourcebits.com>
2012-08-12 12:20:43 +0530

    Last minute checkins

--- lib/trip_advisor.rb
+++ lib/trip_advisor.rb
@@ -17,22 +17,24 @@ module TripAdvisor
 	      result = JSON.parse(data)
 	    end		
 
-	    def search_places_of_interest(lat_long,query=[], limit=20)
+	    def search_places_of_interest(lat_long)
           client = Foursquare2::Client.new(:client_id => 'I4LODILPNSH1KSHBGSMCNCIO3IFCZDMPCBJT2CBMIUWVW2C1', 
           	                               :client_secret => 'XJWCWDSZLMDKXHSYXT5QXC15GP1JIJZWLDJKRXMXSB4DK2DA')
           #client.search_venues(:ll => '36.142064,-86.816086', :query => 'Starbucks')
-          interest_areas = client.search_venues(:ll => lat_long, :query => query, :limit => limit)
+          interest_areas = client.search_venues(:ll => lat_long)
           
           arr = []
           hash_arr = {}	
-          
-          interest_areas.first[1].first["items"].each do |u|
-          	hash_arr["distance"] = u.location.distance
-            hash_arr["name"] = u.categories[0]["name"]
-            hash_arr["tag"] = u.categories[0]["parents"]
-            arr << hash_arr
-          end	
+          if interest_areas.present?
+            interest_areas.first[1].first["items"].each do |u|
+            	hash_arr["distance"] = u.location.distance if u.location.present
+              hash_arr["name"] = u.categories[0].present? ? u.categories[0]["name"] : nil
+              hash_arr["tag"] =   u.categories[0].present? ? u.categories[0]["parents"] : nil
+              arr << hash_arr
+            end	
+          end  
           arr
+
 	    end  	
 
     end
