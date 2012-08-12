5628f398f99e90aad5ee371f2fd7a56cd7929f28
bijendra <bijendra@sourcebits.com>
2012-08-12 07:29:49 +0530

    Areas of interest

--- lib/trip_advisor.rb
+++ lib/trip_advisor.rb
@@ -3,6 +3,10 @@ module TripAdvisor
 	require 'json'
 	
 	class << self
+
+		require 'uri'
+    	require 'json'
+    	require 'net/http'
         
         #enter the locations to get the list of hotels or enter the hotel name.
 	    def hotels_search(query)
@@ -18,8 +22,11 @@ module TripAdvisor
 		   return result
 	    end		
 
-	    def search_places_of_interest()
-          
+	    def search_places_of_interest(lat_long,query=[], limit=20)
+          client = Foursquare2::Client.new(:client_id => 'I4LODILPNSH1KSHBGSMCNCIO3IFCZDMPCBJT2CBMIUWVW2C1', 
+          	                               :client_secret => 'XJWCWDSZLMDKXHSYXT5QXC15GP1JIJZWLDJKRXMXSB4DK2DA')
+          #client.search_venues(:ll => '36.142064,-86.816086', :query => 'Starbucks')
+          client.search_venues(:ll => 'lat_long', :query => query, :limit => limit)
 	    end  	
 
     end
