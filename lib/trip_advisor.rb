89199b0f5a66bc796fd664a93e356a73b276c554
bijendra <bijendra@sourcebits.com>
2012-08-12 13:38:56 +0530

    Change the queries count

--- lib/trip_advisor.rb
+++ lib/trip_advisor.rb
@@ -21,17 +21,19 @@ module TripAdvisor
           client = Foursquare2::Client.new(:client_id => 'I4LODILPNSH1KSHBGSMCNCIO3IFCZDMPCBJT2CBMIUWVW2C1', 
           	                               :client_secret => 'XJWCWDSZLMDKXHSYXT5QXC15GP1JIJZWLDJKRXMXSB4DK2DA')
           #client.search_venues(:ll => '36.142064,-86.816086', :query => 'Starbucks')
-          interest_areas = client.search_venues(:ll => lat_long)
+          interest_areas = client.search_venues(:ll => lat_long,:limit => 50, :radius => 3000, :section => "outdoors")
           
           arr = []
           hash_arr = {}	
           if interest_areas.present?
-            interest_areas.first[1].first["items"].each do |u|
-            	hash_arr["distance"] = u.location.distance if u.location.present
-              hash_arr["name"] = u.categories[0].present? ? u.categories[0]["name"] : nil
-              hash_arr["tag"] =   u.categories[0].present? ? u.categories[0]["parents"] : nil
-              arr << hash_arr
-            end	
+            if interest_areas.first[1].blank?
+              interest_areas.first[1].first["items"].each do |u|
+              	hash_arr["distance"] = u.location.distance if u.location.present
+                hash_arr["name"] = u.categories[0].present? ? u.categories[0]["name"] : nil
+                hash_arr["tag"] =   u.categories[0].present? ? u.categories[0]["parents"] : nil
+                arr << hash_arr
+              end	
+            end  
           end  
           arr
 
